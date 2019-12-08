ea4bf2bfd07f5d2ca6f82a6a7ac7a054bf519eeb
Jakub Ladyga <jakub.ladyga@microsoft.wsei.edu.pl>
2019-12-08 15:52:49 +0100

    Added answers to DOM_elements

--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -9,4 +9,55 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania
      */
 
+     //Zadanie 0 
+     function getDatasInfo(elements){
+         var data = [];
+
+         for(var i =0; i <elements.length; i++){
+             data.push(elements[i].dataset.color);
+             }
+            return data;
+         }
+     
+         console.log(getDatasInfo(links));
+
+         //Zadanie1
+         console.log(homeElement);
+
+         for(let item  of childElements) console.log(item);
+
+         console.log(banner) 
+
+         for(let item of blocks) console.log(item);
+
+         for(let item of links) console.log(item);
+         
+         //Zadanie2
+         for(let item of blocks){
+             console.log(item.innerHTML);
+             console.log(item.outerHTML);
+         }
+
+         //Zadanie2/2 
+
+         var newBlocks = document.querySelector('blocks');
+         console.log(newBlocks.innerHTML);
+         console.log(newBlocks.outerHTML);
+        
+        
+         //Zadanie3
+        const footer = document.querySelector('#mainFooter');
+
+        function getId(element){
+             return element.id;
+        }
+
+        //Zadanie4
+        function  getTags(elements){
+            const tagsNames = [];
+            for(let item of elements){
+                 tagsNames.push(item.tagName);
+            }
+            return tagsNames;
+        }
 });
